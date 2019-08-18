aad8ff9f09d82883367e7a7138c27fe856b5a93c
Ilir Pllana <Ipllana7382@conestogac.on.ca>
2019-08-17 22:15:41 -0400

    first commit

---
+++ IPllanaAssignment2,/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace IPllanaAssignment2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pl2 = new System.Windows.Forms.PictureBox();
+            this.pl1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.Transparent;
+            this.btn9.Location = new System.Drawing.Point(363, 468);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(153, 158);
+            this.btn9.TabIndex = 17;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.Transparent;
+            this.btn8.Location = new System.Drawing.Point(195, 468);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(153, 158);
+            this.btn8.TabIndex = 16;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Transparent;
+            this.btn7.Location = new System.Drawing.Point(26, 468);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(153, 158);
+            this.btn7.TabIndex = 15;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Transparent;
+            this.btn6.Location = new System.Drawing.Point(363, 292);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(153, 158);
+            this.btn6.TabIndex = 14;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Transparent;
+            this.btn5.Location = new System.Drawing.Point(195, 292);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(153, 158);
+            this.btn5.TabIndex = 13;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Transparent;
+            this.btn4.Location = new System.Drawing.Point(26, 292);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(153, 158);
+            this.btn4.TabIndex = 12;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Transparent;
+            this.btn2.Location = new System.Drawing.Point(195, 128);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(153, 152);
+            this.btn2.TabIndex = 10;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Transparent;
+            this.btn1.Location = new System.Drawing.Point(26, 128);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(153, 152);
+            this.btn1.TabIndex = 9;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("3ds", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(550, 532);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 25);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Player2:";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("3ds", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(550, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 30);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Player1:";
+            // 
+            // pl2
+            // 
+            this.pl2.Image = global::IPllanaAssignment2_.Properties.Resources.O;
+            this.pl2.InitialImage = null;
+            this.pl2.Location = new System.Drawing.Point(656, 468);
+            this.pl2.Name = "pl2";
+            this.pl2.Size = new System.Drawing.Size(156, 155);
+            this.pl2.TabIndex = 20;
+            this.pl2.TabStop = false;
+            // 
+            // pl1
+            // 
+            this.pl1.Image = global::IPllanaAssignment2_.Properties.Resources.X;
+            this.pl1.InitialImage = null;
+            this.pl1.Location = new System.Drawing.Point(656, 131);
+            this.pl1.Name = "pl1";
+            this.pl1.Size = new System.Drawing.Size(156, 149);
+            this.pl1.TabIndex = 19;
+            this.pl1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("3ds", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(843, 27);
+            this.menuStrip1.TabIndex = 22;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(49, 23);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(162, 24);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(57, 23);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("3ds", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(35, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(534, 56);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Welcome to Tic Tac Toe";
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Transparent;
+            this.btn3.Location = new System.Drawing.Point(363, 128);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(153, 152);
+            this.btn3.TabIndex = 24;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(843, 686);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pl2);
+            this.Controls.Add(this.pl1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pl1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pl2;
+        private System.Windows.Forms.PictureBox pl1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+    }
+}
+
---
+++ IPllanaAssignment2,/Form1.cs
@@ -0,0 +1,383 @@
+﻿// ILIR PLLANA
+// ASSIGNMENT 2 - TIC TAC TOE GAME
+// STEVE HENDRIKSE - GAME OBJECT ORIENTED
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace IPllanaAssignment2_
+{
+    public partial class Form1 : Form
+    {
+        bool playerturn = true;
+        bool[,] xindex ={ { true, true, true }, { true, true, true }, { true, true, true} };
+        bool[,] oindex = { { false, false, false }, { false, false, false }, { false, false, false} };
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+      
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Check if O or X wins: diagonal, vertical, horizontal ./
+        public void checkWinner()
+        {
+            
+            string calculation1 = xindex[0, 0].ToString() + xindex[0, 1].ToString() + xindex[0, 2].ToString();
+            string calculation2 = oindex[0, 0].ToString() + oindex[0, 1].ToString() + oindex[0, 2].ToString();
+
+            string calculation3 = xindex[1, 0].ToString() + xindex[1, 1].ToString() + xindex[1, 2].ToString();
+            string calculation4 = oindex[1, 0].ToString() + oindex[1, 1].ToString() + oindex[1, 2].ToString();
+
+            string calculation5 = xindex[2, 0].ToString() + xindex[2, 1].ToString() + xindex[2, 2].ToString();
+            string calculation6 = oindex[2, 0].ToString() + oindex[2, 1].ToString() + oindex[2, 2].ToString();
+
+            string calculation7 = xindex[0, 0].ToString() + xindex[1, 0].ToString() + xindex[2, 0].ToString();
+            string calculation8 = oindex[0, 0].ToString() + oindex[1, 0].ToString() + oindex[2, 0].ToString();
+
+            string calculation9 = xindex[0, 1].ToString() + xindex[1, 1].ToString() + xindex[2, 1].ToString();
+            string calculation10 = oindex[0, 1].ToString() + oindex[1, 1].ToString() + oindex[2, 1].ToString();
+
+            string calculation11 = xindex[0, 2].ToString() + xindex[1, 2].ToString() + xindex[2, 2].ToString();
+            string calculation12 = oindex[0, 2].ToString() + oindex[1, 2].ToString() + oindex[2, 2].ToString();
+
+            string calculation13 = xindex[0, 0].ToString() + xindex[1, 1].ToString() + xindex[2, 2].ToString();
+            string calculation14 = oindex[0, 0].ToString() + oindex[1, 1].ToString() + oindex[2, 2].ToString();
+
+            string calculation15 = xindex[2, 0].ToString() + xindex[1, 1].ToString() + xindex[0, 2].ToString();
+            string calculation16 = oindex[2, 0].ToString() + oindex[1, 1].ToString() + oindex[0, 2].ToString();
+            counter++;
+            
+            // calculation array
+
+            string[] cal = {calculation1, calculation2, calculation3,
+                            calculation4, calculation5, calculation6,
+                            calculation7, calculation8, calculation9,
+                            calculation10, calculation11, calculation12,
+                            calculation13, calculation14, calculation15,calculation16};
+
+
+            if (cal[0].Length == 15 || cal[2].Length == 15 || cal[4].Length == 15 || cal[6].Length == 15 || cal[8].Length == 15 || cal[10].Length == 15 || cal[12].Length == 15 || cal[14].Length == 15)
+            {
+
+                MessageBox.Show("X Wins");
+                reset();
+                
+            }
+
+            else if (cal[1].Length == 12 || cal[3].Length == 12 || cal[5].Length == 12 || cal[7].Length == 12 || cal[9].Length == 12 || cal[11].Length == 12 || cal[13].Length == 12 || cal[15].Length == 12)
+            {
+
+                MessageBox.Show("O Wins");
+                reset();
+            }
+
+            else if (counter == 9)
+            {
+                MessageBox.Show("It's a Draw");
+                reset();
+            }
+        }
+
+
+        // X and O Images that are shown on buttons
+       
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (playerturn == true) {
+
+                btn1.BackgroundImage = Properties.Resources.X;
+                btn1.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn1.Enabled = false;
+                xindex[0, 0] = false;
+                checkWinner();
+
+            }
+            else
+            {
+
+                btn1.BackgroundImage = Properties.Resources.O;
+                btn1.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn1.Enabled = false;
+                oindex[0, 0] = true;
+                checkWinner();
+            }
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (playerturn == true)
+            {
+
+                btn2.BackgroundImage = Properties.Resources.X;
+                btn2.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn2.Enabled = false;
+                xindex[0, 1] = false;
+                checkWinner();
+            }
+            else
+            {
+
+                btn2.BackgroundImage = Properties.Resources.O;
+                btn2.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn2.Enabled = false;
+                oindex[0, 1] = true;
+                checkWinner();
+            }
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (playerturn == true)
+            {
+
+                btn3.BackgroundImage = Properties.Resources.X;
+                btn3.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn3.Enabled = false;
+                xindex[0, 2] = false;
+                checkWinner();
+            }
+            else
+            {
+
+                btn3.BackgroundImage = Properties.Resources.O;
+                btn3.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn3.Enabled = false;
+                oindex[0, 2] = true;
+                checkWinner();
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (playerturn == true)
+            {
+
+                btn4.BackgroundImage = Properties.Resources.X;
+                btn4.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn4.Enabled = false;
+                xindex[1, 0] = false;
+                checkWinner();
+
+            }
+            else
+            {
+
+                btn4.BackgroundImage = Properties.Resources.O;
+                btn4.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn4.Enabled = false;
+                oindex[1, 0] = true;
+                checkWinner();
+            }
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+
+
+            if (playerturn == true)
+            {
+
+                btn5.BackgroundImage = Properties.Resources.X;
+                btn5.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn5.Enabled = false;
+                xindex[1, 1] = false;
+                checkWinner();
+            }
+            else
+            {
+
+                btn5.BackgroundImage = Properties.Resources.O;
+                btn5.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn5.Enabled = false;
+                oindex[1, 1] = true;
+                checkWinner();
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (playerturn == true)
+            {
+
+                btn6.BackgroundImage = Properties.Resources.X;
+                btn6.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn6.Enabled = false;
+                xindex[1, 2] = false;
+                checkWinner();
+            }
+            else
+            {
+
+                btn6.BackgroundImage = Properties.Resources.O;
+                btn6.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn6.Enabled = false;
+                oindex[1, 2] = true;
+                checkWinner();
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (playerturn == true)
+            {
+
+                btn7.BackgroundImage = Properties.Resources.X;
+                btn7.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn7.Enabled = false;
+                xindex[2, 0] = false;
+                checkWinner();
+            }
+            else
+            {
+
+                btn7.BackgroundImage = Properties.Resources.O;
+                btn7.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn7.Enabled = false;
+                oindex[2, 0] = true;
+                checkWinner();
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (playerturn == true)
+            {
+
+                btn8.BackgroundImage = Properties.Resources.X;
+                btn8.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn8.Enabled = false;
+                xindex[2, 1] = false;
+                checkWinner();
+            }
+            else
+            {
+
+                btn8.BackgroundImage = Properties.Resources.O;
+                btn8.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn8.Enabled = false;
+                oindex[2, 1] = false;
+                checkWinner();
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (playerturn == true)
+            {
+
+                btn9.BackgroundImage = Properties.Resources.X;
+                btn9.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = false;
+                btn9.Enabled = false;
+                xindex[2, 2] = false;
+                checkWinner();
+            }
+            else
+            {
+
+                btn9.BackgroundImage = Properties.Resources.O;
+                btn9.BackgroundImageLayout = ImageLayout.Stretch;
+                playerturn = true;
+                btn9.Enabled = false;
+                oindex[2, 2] = true;
+                checkWinner();
+            }
+        }
+        // Exit the game on stripMenu
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        
+        // About - on the StripMenu
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developed by Ilir Pllana", "Tic Tac Toe", MessageBoxButtons.OK);
+           
+        }
+
+        // Restart the Game on the StripMenu
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+
+        // when game completes, it starts automatically again to play
+        public void reset()
+        {
+
+            btn1.BackgroundImage = null;
+            btn2.BackgroundImage = null;
+            btn3.BackgroundImage = null;
+            btn4.BackgroundImage = null;
+            btn5.BackgroundImage = null;
+            btn6.BackgroundImage = null;
+            btn7.BackgroundImage = null;
+            btn8.BackgroundImage = null;
+            btn9.BackgroundImage = null;
+
+            counter = 0;
+
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+            playerturn = true;
+
+            for(int a = 0; a<=2; a++)
+            {
+                for (int i = 0; i <= 2; i++)
+                {
+                    xindex[a, i] = true;
+                    oindex[a, i] = false;
+
+
+                }
+            }
+        }
+
+    }
+}
